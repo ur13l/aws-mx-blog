cb121116a61367e8072ffec39b954fa981c8c2f5
Edgar Perez <edgarp.dev@gmail.com>
2020-08-11 20:39:58 -0500

    [NEW] Agregando ScrollingLayout styles

---
+++ src/components/ScrollingLayout.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+import Wrapper from "../styles/layout"
+import Header from "./header"
+
+class ScrollingLayout extends Component {
+  state = {
+    isScrolled: false,
+  }
+
+  handleScroll = () => {
+    const isScreenScrolled = window.scrollY > 10
+    const { isScrolled } = this.state
+    if (isScreenScrolled !== isScrolled) {
+      this.setState({
+        isScrolled: isScreenScrolled,
+      })
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener("scroll", this.handleScroll, { passive: true })
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("scroll", this.handleScroll)
+  }
+
+  render() {
+    const { children, location } = this.props
+    const { isScrolled } = this.state
+    return (
+      <Wrapper>
+        <StaticQuery
+          query={graphql`
+            query SiteTitleQuery {
+              site {
+                siteMetadata {
+                  title
+                }
+              }
+            }
+          `}
+          render={({
+            site: {
+              siteMetadata: { title },
+            },
+          }) => (
+            <Header
+              siteTitle="Test"
+              scrolled={isScrolled}
+              location={location}
+            />
+          )}
+        />
+        <main>{children}</main>
+      </Wrapper>
+    )
+  }
+}
+
+ScrollingLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+  location: PropTypes.string.isRequired,
+}
+
+export default ScrollingLayout
