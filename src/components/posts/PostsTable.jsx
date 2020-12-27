1c1b68564d200760c103b40e7654a9c568e016e0
Edgar <Perez>
2020-12-27 13:47:37 -0600

    [NEW] Agregando PostTable component

---
+++ src/components/posts/PostsTable.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import PropTypes from 'prop-types';
+import SectionTitle from "../common/SectionTitle";
+import PostItem from "../PostItem";
+import Paginator from "../paginator";
+
+const PostsTable = ({ totalNumberOfPages, currentPage, posts }) => {
+  if (!posts || posts.length === 0) {
+    return (
+      <>
+        No hay entradas disponibles
+      </>
+    );
+  }
+
+  return (
+    <>
+      <SectionTitle titleText="entradas recientes" />
+      {posts.map((post, key) => <PostItem post={post} key={key} i={key} />)}
+      <Paginator
+        numPages={totalNumberOfPages}
+        currentPage={currentPage}
+        baseRoute={"/"}
+      />
+    </>
+  );
+}
+
+PostsTable.propTypes = {
+  totalNumberOfPages: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  posts: PropTypes.arrayOf(PropTypes.object).isRequired
+}
+
+export default PostsTable;
