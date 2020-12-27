aa6c849c633baa9bb6ac6341ae29b4c680b8e237
Edgar <Perez>
2020-12-27 12:49:52 -0600

    [NEW] Nuevo SectionTitle component

---
+++ src/components/common/SectionTitle.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { orange } from "../../styles/colors";
+
+const theme = {
+  text_color: orange
+}
+
+const Title = styled.h1`
+  font-size: 1em;
+  color: ${props => props.theme.text_color};
+  text-transform: lowercase;
+`;
+
+const SectionTitle = ({ titleText }) => {
+  return(
+    <Title theme={theme}>
+      {titleText}
+    </Title>
+  )
+};
+
+SectionTitle.propTypes = {
+  titleText: PropTypes.string.isRequired
+};
+
+export default SectionTitle;
