ddfa65fb7cf94264308748d25d1ad8a8dec06fae
Uriel <ur13l.infante@gmail.com>
2021-02-27 17:59:31 -0600

    [UPDATE] Data

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PageLayout from './page-layout';
 import styled from 'styled-components';
 import 'react-multi-carousel/lib/styles.css';
